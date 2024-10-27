0b3983f76154197c0d5a84c1e5f5565465610f23
Druvamk <130459817+Druvamk@users.noreply.github.com>
2024-10-27 21:52:02 +0530

    react-18

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,31 @@
+import { ChangeEvent, useState, useTransition } from "react";
+
+const items = ["druva", "Sanju", "Chaitu", "Darshan", "Chitra", "Cheta"];
+
 export default function App() {
-  return <div></div>;
+  const [data, setData] = useState(items);
+  const [textInput, setTextInput] = useState("");
+  const [isPending, startTransition] = useTransition();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    setTextInput(inputValue);
+
+    startTransition(() => {
+      const filteredData = items.filter((item) =>
+        item.toLowerCase().includes(inputValue.toLowerCase())
+      );
+      setData(filteredData);
+    });
+  };
+
+  return (
+    <div>
+      {isPending && <p>Loading...</p>}
+      <input type="text" value={textInput} onChange={handleChange} />
+      {data.map((item, i) => (
+        <div key={i}>{item}</div>
+      ))}
+    </div>
+  );
 }
