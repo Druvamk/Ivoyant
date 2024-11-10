3e6ab72f082479336c2226b1090ea1992938118e
Druvamk <130459817+Druvamk@users.noreply.github.com>
2024-11-10 20:20:49 +0530

    todo-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,58 @@
+import { FormEvent, useState } from "react";
+interface Todo {
+  id: number;
+  text: string;
+  complete: boolean;
+}
 export default function App() {
-  return <div></div>;
+  const [text, setText] = useState<string>("");
+  const [todos, setTodos] = useState<Array<Todo>>([]);
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    setTodos([...todos, { id: Date.now(), text, complete: false }]);
+    setText("");
+  }
+  function handleDelete(id: number) {
+    setTodos(todos.filter((item) => item.id !== id));
+  }
+  function handleCheckBox(id: number) {
+    setTodos(
+      todos.map((item) =>
+        item.id === id ? { ...item, complete: !item.complete } : item
+      )
+    );
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Enter the text..."
+        />
+        <button type="submit">Add</button>
+      </form>
+      <div>
+        {todos.map((item, i) => (
+          <div key={i}>
+            <input
+              type="checkbox"
+              checked={item.complete}
+              onChange={() => handleCheckBox(item.id)}
+              placeholder="checkin"
+            />
+            <span
+              style={{
+                textDecoration: item.complete ? "line-through" : "none",
+              }}
+            >
+              {item.text}
+            </span>
+            <button onClick={() => handleDelete(item.id)}>❌</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
